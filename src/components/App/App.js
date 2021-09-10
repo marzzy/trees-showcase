56be4333b977626575de30ed54b4f92b7abfb521
marzzy mogh <marzzy.m@gmail.com>
2021-09-10 15:44:36 +0430

    feat: fix the basic structure of project

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import logo from 'assets/logo60.png';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <span>
+          Tree Showcase
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
